89896d17de7e6e8bf3c4061dc028f9eb21b19795
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-13 04:58:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,3 @@
 # Write your code here.
-def line(array)
-  if array.length >= 1 
-    yee = []
-    counter = 1 
-    array.each do |name|
-      yee<<"#{counter}. #{name}"
-      counter += 1 
-    end
-  puts "The line is currently: #{yee.join(" "}."
-  else
-    puts "The line is currently empty."
-  end
-end
+
 
