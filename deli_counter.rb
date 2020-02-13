d6d64bb3da007b65fd4bb8b49a67937c95fce39f
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-13 04:49:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,7 @@
 # Write your code here.
 def line(array)
-  if array == []
+  if array.length == 0
     puts "The line is currently empty."
   end
 end
 
-def take_a_number(array, name)
-  count = 1 
-  puts "The line is currently"
-  array.each do |yee|
-    puts #{count}. #{name}"
-  count += 1
-  end
-end
